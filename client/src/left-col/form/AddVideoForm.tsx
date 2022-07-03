d2c3c7b29ee0ff59f77e25aff73c352dd290884a
alon <alon@ebby.co>
2022-07-03 16:56:02 +0300

    client: AddVideoForm.tsx
    Fixed: placehoder now properly describes the expected input
    Submit button is disabled when socket is disconnected

--- client/src/left-col/form/AddVideoForm.tsx
+++ client/src/left-col/form/AddVideoForm.tsx
@@ -1,15 +1,19 @@
 import {Button, Col, Form, FormGroup, Input, Label} from "reactstrap";
+import {useContext} from "react";
+import {ConnectionContext} from "../../connection-context/ConnectionContextProvider";
 
 const AddVideoForm = () => {
+    const connection = useContext(ConnectionContext);
+
     return (
         <Form>
             <FormGroup row>
                 <Col xs={9} >
                     <Label for="videoId" hidden>Video Id</Label>
-                    <Input type="text" name="videoId" id="videoId" placeholder="Enter Video Id" />
+                    <Input type="text" name="videoId" id="videoId" placeholder="Enter YouTube Video URL" />
                 </Col>
                 <Col>
-                    <Button type="submit" color="primary">Add</Button>
+                    <Button type="submit" color="primary" disabled={!(!!connection?.isConnected)}>Add</Button>
                 </Col>
             </FormGroup>
         </Form>
