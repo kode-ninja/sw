66c9189aef4f3e9b05c95371ebdbb7216c5a2c31
alon <alon@ebby.co>
2022-07-03 16:57:58 +0300

    client: App.tsx to use both Context providers

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,31 +3,37 @@ import {Col, Container, Row} from "reactstrap";
 import LeftColumn from "../left-col/LeftColumn";
 import useUserMessage from "../user-message/useUserMessage";
 import {TUserMessageText} from "../user-message/interfaces";
+import ConnectionContextProvider from "../connection-context/ConnectionContextProvider";
+import PlaylistContextProvider from "../playlist-context/PlaylistContextProvider";
 
 interface IUserMessageManagerContextValue {
     showUserSuccessMessage: (messageText: TUserMessageText, displayDuration?: number) => void,
     showUserErrorMessage: (messageText: TUserMessageText, displayDuration?: number) => void,
 }
 
-export const UserMessageManager = createContext<IUserMessageManagerContextValue | null>(null);
+export const UserMessageContext = createContext<IUserMessageManagerContextValue | null>(null);
 
 function App() {
     const {renderUserMessage, ...restOfUserMessageManager} = useUserMessage();
 
     return (
-        <UserMessageManager.Provider value={restOfUserMessageManager}>
-            <Container className="mt-5">
-                <Row>
-                    <Col xs={12} lg={4}>
-                        <LeftColumn/>
-                    </Col>
-                    <Col xs={12} lg={8} className="order-first order-lg-last">
+        <UserMessageContext.Provider value={restOfUserMessageManager}>
+            <ConnectionContextProvider>
+                <PlaylistContextProvider>
+                    <Container className="mt-5">
+                        <Row>
+                            <Col xs={12} lg={4}>
+                                <LeftColumn/>
+                            </Col>
+                            <Col xs={12} lg={8} className="order-first order-lg-last">
 
-                    </Col>
-                </Row>
-            </Container>
-            {renderUserMessage()}
-        </UserMessageManager.Provider>
+                            </Col>
+                        </Row>
+                    </Container>
+                    {renderUserMessage()}
+                </PlaylistContextProvider>
+            </ConnectionContextProvider>
+        </UserMessageContext.Provider>
     );
 }
 
